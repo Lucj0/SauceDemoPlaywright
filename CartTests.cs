71fda9674b82a2e57422607166ee12c3e5f3d077
Alvin Gwak <alvingwak@gmail.com>
2026-09-07 16:23:13 -0700

    Add cart tests: remove to empty, targeted removal, multi-item badge count

--- CartTests.cs
+++ CartTests.cs
@@ -50,4 +50,62 @@ public class CartTests : PageTest
         await Expect(Page).ToHaveURLAsync("https://www.saucedemo.com/checkout-complete.html");
         await Expect(Page.Locator("[data-test='complete-text']")).ToBeVisibleAsync();
     }
+
+    [Test]
+    public async Task RemoveItemFromCart_CartEmpty()
+    {
+        //Arrange
+        var loginPage = new LoginPage(Page);
+        var inventoryPage = new InventoryPage(Page);
+        var cartPage = new CartPage(Page);
+        await loginPage.GotoAsync();
+
+        //Act
+        await loginPage.LoginAsync("standard_user", "secret_sauce");
+        await inventoryPage.AddItemAsync("add-to-cart-sauce-labs-bike-light");
+        await inventoryPage.GoToCartAsync();
+        await cartPage.RemoveItemFromCartAsync("remove-sauce-labs-bike-light");
+
+        //Assert
+        await Expect(Page.Locator("[data-test='inventory-item']")).ToHaveCountAsync(0);
+    }
+
+
+    [Test]
+    public async Task RemoveOneItemFromCart_OtherItemsUnaffected()
+    {
+        //Arrange
+        var loginPage = new LoginPage(Page);
+        var inventoryPage = new InventoryPage(Page);
+        var cartPage = new CartPage(Page);
+        await loginPage.GotoAsync();
+
+        //Act
+        await loginPage.LoginAsync("standard_user", "secret_sauce");
+        await inventoryPage.AddItemAsync("add-to-cart-sauce-labs-bike-light");
+        await inventoryPage.AddItemAsync("add-to-cart-sauce-labs-backpack");
+        await inventoryPage.GoToCartAsync();
+        await cartPage.RemoveItemFromCartAsync("remove-sauce-labs-bike-light");
+
+        //Assert
+        await Expect(Page.Locator("[data-test='inventory-item']")).ToHaveCountAsync(1);
+        await Expect(Page.Locator("[data-test='inventory-item-name']")).ToContainTextAsync("Sauce Labs Backpack");
+    }
+
+    [Test]
+    public async Task AddTwoItemsToCart_CartBadgeShowsTwo()
+    {
+        //Arrange
+        var loginPage = new LoginPage(Page);
+        var inventoryPage = new InventoryPage(Page);
+        await loginPage.GotoAsync();
+
+        //Act
+        await loginPage.LoginAsync("standard_user", "secret_sauce");
+        await inventoryPage.AddItemAsync("add-to-cart-sauce-labs-bike-light");
+        await inventoryPage.AddItemAsync("add-to-cart-sauce-labs-backpack");
+
+        //Assert
+        await Expect(Page.Locator("[data-test='shopping-cart-badge']")).ToContainTextAsync("2");
+    }
 }
